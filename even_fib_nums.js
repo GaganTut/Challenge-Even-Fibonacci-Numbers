ea5a7e3842151a262953bf66ef767d59dc4b2593
GaganTut <gstut@live.com>
2017-03-15 09:26:48 -1000

    Passed the four tests

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,8 +7,16 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+  var fibSeq = [0,1];
 
-  // do your work here
+  while (maxFibValue > fibSeq[fibSeq.length - 1]) {
+    fibSeq.push(fibSeq[fibSeq.length - 1] + fibSeq[fibSeq.length -2]);
+  }
+  for (var i = 0; i < fibSeq.length; i++) {
+    if (fibSeq[i] % 2 === 0) {
+      sum += fibSeq[i];
+    }
+  }
 
   return sum;
 }
@@ -18,12 +26,17 @@ function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
   //define your base case, validate your input
+  var fibSeq = [0,1];
 
+  while (maxFibValue > fibSeq[fibSeq.length - 1]) {
+    fibSeq.push(fibSeq[fibSeq.length - 1] + fibSeq[fibSeq.length -2]);
+  }
+  console.log(fibSeq);
 
+  highest = fibSeq[fibSeq.length - 2];
   //do your work here
-
   return highest;
-};
+}
 
 /**
  * Do not modify code below.
